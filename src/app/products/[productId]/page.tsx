127852bdec1838b1c9e81afa712b874fe666bdcb
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-08 23:49:55 +0530

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,23 @@
-export default function Product({
-  params,
-}: {
+import { Metadata } from "next";
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+export default function ProductDetails({ params }: Props) {
   console.log("params", params.productId);
   return <h1>Details about Product {params.productId}</h1>;
 }
