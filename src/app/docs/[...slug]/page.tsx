522b36ea936ec8ce8eded78158146c77c16ee610
AbhishekTiwari <abhishek.tiwari@firsthive.com>
2025-01-08 22:05:20 +0530

    added not-found.tsx and notFound() function

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -2,7 +2,7 @@ export default function Docs({ params }: { params: { slug: string[] } }) {
   if (params.slug.length === 2) {
     return (
       <div>
-        Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
+        Viewing docs for {params.slug[0]} and concept {params.slug[1]}
       </div>
     );
   } else if (params.slug.length === 1) {
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,16 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({
   params,
 }: {
   params: { reviewId: string; productId: string };
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <h1>
-      Review {params.reviewId} for Product {}
+      Review {params.reviewId} for Product
       {params.productId}
     </h1>
   );
